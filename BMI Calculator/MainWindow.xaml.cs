42ff90be203b6a61826beea262a245edccec0238
MisterMagik <denis.panek2004@op.pl>
2026-05-20 12:24:29 +0200

    Add project files.

---
+++ BMI Calculator/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BMI_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private double height;
+        private double weight;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(HeightTextBox.Text, out height) && double.TryParse(WeightTextBox.Text, out weight))
+            {
+                double bmi = weight / (height * height);
+                ResultLabel.Content = $"BMI: {bmi}";
+            }
+            else
+            {
+                MessageBox.Show("Wpisz poprawne wartości dla wzrostu i wagi.", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void ResetButtonClick(object sender, RoutedEventArgs e)
+        {
+            HeightTextBox.Clear();
+            WeightTextBox.Clear();
+            ResultLabel.Content = string.Empty;
+        }
+    }
+}
